87d087a3ac39300c9ea98bbd8aa3af4ca7b9e514
kristyk2032 <korchaginakristyna@yandex.ru>
2025-04-26 14:52:51 +0300

    Add path length calculation to algorithm

--- dfs.py
+++ dfs.py
@@ -1,19 +1,23 @@
-def dfs(graph, start_node):
-    visited = []
-    stack = [start_node]
+def dfs_path_length(graph, start_node, end_node):
+    visited = set()
+    stack = [(start_node, 0)]  
 
     while stack:
-        node = stack.pop()
+        node, path_length = stack.pop()
+        if node == end_node:
+            return path_length 
         if node not in visited:
-            visited.append(node)
+            visited.add(node)
             neighbors = graph[node] if node in graph else []
-            stack.extend(neighbors)
+            for neighbor in neighbors:
+                stack.append((neighbor, path_length + 1))
 
-    return visited
+    return -1  
 
 graph = {1: [3], 2: [4], 4: [2]}
 
-start_node = 1
-path = dfs(graph, start_node)
-print(f"DFS path from node {start_node}: {path}") 
+start_node = 2
+end_node = 4
 
+path_length = dfs_path_length(graph, start_node, end_node)
+print(f"Path length from {start_node} to {end_node}: {path_length}")  
